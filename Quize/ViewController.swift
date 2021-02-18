9daf5c1c6f6d344d4504e3fcb3d5212c14478a7f
Kony <fzf55747@kwansei.ac.jp>
2021-02-19 04:27:29 +0900

    Initial Commit

---
+++ Quize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quize
+//
+//  Created by 小西星七 on 2021/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
